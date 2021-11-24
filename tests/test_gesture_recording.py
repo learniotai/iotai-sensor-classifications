d26104fb38140f070f5a449baf15645ae6405350
Learn IoTAI <learniotai@gmail.com>
2021-11-24 09:41:07 -0500

    Formatting

--- tests/test_gesture_recording.py
+++ tests/test_gesture_recording.py
@@ -1,3 +1,4 @@
+"""Test loading gesture recordings and visualizing data."""
 from iotai_sensor_classification.gesture_recording import read_gesture_recordings
 
 from data.gestures import linear_accelerometer
@@ -11,9 +12,11 @@ def test_parse_motions():
     gestures = gesture_recording.keys()
     assert "shake" in gestures
     assert "rock" in gestures
-    test_output = os.path.join("test_output", "gesture_recordings")
+    test_output = os.path.join("test_output", "gestures", "raw")
     os.makedirs(test_output, exist_ok=True)
     for gesture in gestures:
         gesture_data = gesture_recording[gesture]
-        column_histograms(gesture_data, name=f"{gesture} gesture", filepath=os.path.join(test_output, f"{gesture}-histograms.png"))
-        plot_columns(gesture_data, name=f"{gesture} gesture", filepath=os.path.join(test_output, f"{gesture}-plots.png"))
+        column_histograms(gesture_data, name=f"{gesture} gesture",
+                          filepath=os.path.join(test_output, f"{gesture}-histograms.png"))
+        plot_columns(gesture_data, name=f"{gesture} gesture",
+                     filepath=os.path.join(test_output, f"{gesture}-plots.png"))
